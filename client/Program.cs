a059d9b4ae1819d483531c7acb3aaa0da4cbff67
G Door <greg.door@gmail.com>
2016-05-17 21:42:47 -0700

    Initial hello-world-ish skeleton of a service

---
+++ client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            RunAsync().Wait(5 * 60 * 1000);
+
+            Console.ReadKey();
+
+            return;
+        }
+
+        private static async Task RunAsync()
+        {
+            if (Debugger.IsAttached)
+            {
+                // If debugging, give the service time to start up. 
+                await Task.Delay(10 * 1000);
+            }
+
+            Uri baseUrl = new Uri("http://localhost:10577/");
+
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = baseUrl,
+                Timeout = TimeSpan.FromSeconds(30),
+            };
+
+            var response = await client.GetAsync("api/values");
+
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
